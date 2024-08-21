d7bc0afd3eca4b8fca70395af7625d591d9c4e46
Pam Van Londen <pam.vanlonden@oregonstate.edu>
2024-08-21 13:27:46 -0700

    render new method learn with jon

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    server: {
+    proxy: {
+    '/arts': { 
+        target: 'http://localhost:3002', 
+        changeOrigin: true, 
+        secure: false,
+      }
+    }
+  }
+})
+
---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import {} from './models-art.mjs';
+
+import path from 'path';
+import {fileURLToPath } from "url";
+
+// Resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(__dirname)
+
+// Initializing Express app
+const app = express();
+
+// Middleware to receive JSON
+app.use(express.json());
+
+// API endpoints  ? and route handlers
+app.use("/arts", );
+
+// Use the client app
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res) => 
+    res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+);
+
+// Connecting to MongoDB using Mongoose
+mongoose
+  .connect("mongodb://localhost:3002", { dbName: "mydbs" })
+  .then(() => {
+    // Listening to requests if DB connection is successful
+    app.listen(3002, "localhost", () => console.log("Listening to port 3002"));
+    console.log('Successfully connected to the Arts collection on the MongoDB Atlas Cluster.');
+  })
+  .catch((err) => console.log(err));
